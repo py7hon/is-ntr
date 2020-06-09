fdb24320bd5a2e9df4c6f790d1317e38db9292f1
Iqbal Rifai <iqbl@nhent.ai>
2020-06-09 17:32:34 +0700

    export from https://glitch.com/~is-ntr

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const axios = require("axios");
+
+app.set("view engine", "pug");
+
+app.use(express.static("public"));
+
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/g/:code([0-9]{1,6})", async (request, response) => {
+  const { data } = await axios.get(
+    `https://nhentai.net/api/gallery/${request.params.code}`
+  );
+  response.render("template", data);
+});
+
+const listener = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
